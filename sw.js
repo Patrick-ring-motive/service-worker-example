7f7316787fd2a25d936fef3d2a7c0f6a90f4c184
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-08 11:40:13 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -146,13 +146,14 @@ const loosest = {
             }
           }
           /* Don't turn off Service Worker until this is done */
-          const response = await awaitUntil(event,offFirstFetch(request));
+          const rep = awaitUntil(event,offFirstFetch(request))
+          const response = await rep;
           if(response && (response instanceof Response)){
             event.respondWith(response.clone());
           }else{
             console.log(response);
           }
-          event.waitUntil(response);
+          event.waitUntil(rep);
           return;
         }
         /* HTML files */
@@ -173,13 +174,14 @@ const loosest = {
             }
           }
           /* Don't turn off Service Worker until this is done */
-          const response = await awaitUntil(event,netFirstFetch(request));
+          const rep = awaitUntil(event,offFirstFetch(request))
+          const response = await rep;
           if(response && (response instanceof Response)){
             event.respondWith(response.clone());
           }else{
             console.log(response);
           }
-          event.waitUntil(response);
+          event.waitUntil(rep);
           return;
         }
       })());
