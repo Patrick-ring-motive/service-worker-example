7ff297250647d71e91496366220deb635a99011a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-12-11 13:19:40 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
 async function importJSDOM(){
-  return globalThis.jsdom ?? (await(await import('https://cdn.jsdelivr.net/npm/jsdom-bundle@1.0.6/bundles/gzimport.js')).jsdomImport);
+  return globalThis.jsdom ?? (await(async function jsdomImport(){
+  eval?.(await (await fetch('https://cdn.jsdelivr.net/npm/pako@2.1.0/dist/pako.min.js')).text());
+  const pako = globalThis.pako;
+  const data = await fetch('https://raw.githubusercontent.com/Patrick-ring-motive/jsdom-bundle/refs/heads/main/bundles/kid-index.js.gz');
+  const stream = data.body;
+  const inflator = new pako.Inflate();
+  const decoder = new TextDecoder();
+  for await (const chunk of stream) {
+    inflator.push(chunk);
+  }
+  const output = inflator.result;
+  eval?.(decoder.decode(output));
+  return globalThis.jsdom;
+})());
 }
 void async function(){
   await importJSDOM();
